258aa65fd0bf407c6731453cd6771de1a09c743d
sfinoteffy <stephane.finot@effy.fr>
2024-01-21 16:38:06 +0100

    add view transition router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { BasicComponent } from './app/basic/basic.component';
 import { CardListsComponent } from './app/card-lists/card-lists.component';
@@ -10,12 +10,15 @@ import { ImagesComponent } from './app/images/images.component';
 bootstrapApplication(AppComponent, {
     providers: [
       importProvidersFrom(BrowserModule),
-      provideRouter([
-        {path: '', pathMatch: 'full', redirectTo: '/images'},
-        {path: 'images', component: ImagesComponent},
-        {path: 'card-lists', component: CardListsComponent},
-        {path: 'basic', component: BasicComponent},
-      ])
+      provideRouter(
+        [
+          {path: '', pathMatch: 'full', redirectTo: '/images'},
+          {path: 'images', component: ImagesComponent},
+          {path: 'card-lists', component: CardListsComponent},
+          {path: 'basic', component: BasicComponent},
+        ],
+        withViewTransitions()
+      )
     ]
 })
   .catch(err => console.error(err));
