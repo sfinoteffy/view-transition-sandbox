127efa53b18c0e2d76b7ebebc94d4bcbaea348ad
sfinoteffy <stephane.finot@effy.fr>
2024-01-21 16:30:08 +0100

    standalone migration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { fromEvent, take } from 'rxjs';
-import { IMAGES } from './images/config';
-import { ViewTransition } from './transition.model';
-import { BasicComponent } from './basic/basic.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [BasicComponent]
+    imports: [RouterOutlet]
 })
-export class AppComponent {
-  
-
-  
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BasicComponent } from './app/basic/basic.component';
+import { CardListsComponent } from './app/card-lists/card-lists.component';
+import { ImagesComponent } from './app/images/images.component';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+    providers: [
+      importProvidersFrom(BrowserModule),
+      provideRouter([
+        {path: '', pathMatch: 'full', redirectTo: '/images'},
+        {path: 'images', component: ImagesComponent},
+        {path: 'card-lists', component: CardListsComponent},
+        {path: 'basic', component: BasicComponent},
+      ])
+    ]
 })
   .catch(err => console.error(err));
